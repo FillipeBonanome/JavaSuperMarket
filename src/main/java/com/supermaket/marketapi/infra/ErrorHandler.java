49ec0fbbdbcb25e9f0a9e21c2d81d6161dbf504b
FillipeBonanome <incarnialol2@gmail.com>
2025-05-11 16:08:42 -0300

    Added argument error handler
    - New handler for wrong arguments

--- src/main/java/com/supermaket/marketapi/infra/ErrorHandler.java
+++ src/main/java/com/supermaket/marketapi/infra/ErrorHandler.java
@@ -4,15 +4,25 @@ import com.supermaket.marketapi.dtos.ErrorDTO;
 import com.supermaket.marketapi.exception.ProductException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(ProductException.class)
-    public ResponseEntity<ErrorDTO> HandleProductException(ProductException exception) {
+    public ResponseEntity<ErrorDTO> handleProductException(ProductException exception) {
         return ResponseEntity.badRequest().body(new ErrorDTO(ProductException.class.getSimpleName(), exception.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorDTO>> handleArgumentError(MethodArgumentNotValidException exception) {
+        List<FieldError> errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(e -> new ErrorDTO(e.getField(), e.getDefaultMessage())).toList());
     }
 
 }
